76be7d2ec48787f593730beedd891886da220b30
hubert <kudik233@gmail.com>
2023-07-20 23:30:37 +0200

    only Dense without corections (working only for 2 classes, old structure) added(batches)

--- mlp.py
+++ mlp.py
@@ -13,11 +13,8 @@ from tqdm import tqdm
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-
-
-
-
-
+import tensorflow as tf
+mse = tf.keras.losses.MeanSquaredError()
 def normalize(arr):
     return (arr - arr.min()) / (arr.max() - arr.min())
 
@@ -30,61 +27,32 @@ def simoid(s):
 
 
 
-
-
-
-class Linear:
-    def __init__(self, neurony, wejscia):
-        self.m_w = np.random.randn(wejscia, neurony)
-        self.v_b = np.zeros((1, neurony))
-        self.v_X = None
-
-    def forward(self, v_x):
-        self.v_X = v_x
-
-        return (self.v_X @ self.m_w) + self.v_b
-
-    def backward(self, blad, eta):
-        self.m_w -= eta * (self.v_X.T @ blad)
-        self.v_b -= eta * blad
-        return blad @ self.m_w.T
-
-class Activation:
-    def __init__(self):
-        self.s = None
-
-    def forward(self,s):
-        self.s = s
-
-        return logistic(self.s)
-
-    def backward(self, blad,eta):
-        return simoid(logistic(self.s))*blad
-
-class Layer:
+class Model:
     def __init__(self):
         self.warstwa = []
 
     def nowa_warstwa(self,neurony,wejscia):
         self.warstwa.append(Linear(neurony,wejscia))
         self.warstwa.append(Activation())
-    def learn(self,epoki,X,Y):
+
+    def learn(self,epoki,X,Y,batch = 1,eta = 0.1):
         for a in tqdm(range(epoki)):
-            for i in range(len(X)):
-                wyjscie = X[i].reshape(1, -1)
+            for i in range(int(len(X)/batch)):
+                wyjscie = X[i*batch:i*batch+batch]
+
                 for j in self.warstwa:
                     wyjscie = j.forward(wyjscie)
 
-                blad = 2 * (wyjscie - Y[i])/Y.size
-
-
+                #blad = 2 * (wyjscie - np.array([Y[i*batch:i*batch+batch]]).T)/Y.size
+                print(wyjscie,np.array([Y[i*batch:i*batch+batch]]).T,Y.size,'ddddddd')
+                blad = (np.sum((wyjscie - np.array([Y[i*batch:i*batch+batch]]).T)**2))/batch
+                blad1 = mse(wyjscie,Y[i*batch:i*batch+batch]).numpy()
+                print(blad,blad1)
+                # print(blad, 'blad')
                 for j in reversed(self.warstwa):
-                    blad = j.backward(blad, eta=0.1)
+                    blad = j.backward(blad, eta=eta)
 
     def predict(self, X):
-
-
-
         # for i in range(len(X)):
         #     wyjscie = X[i].reshape(1, -1)
         #     for j in self.warstwa:
@@ -99,4 +67,36 @@ class Layer:
                 wyjscie = layer.forward(wyjscie)
             wyjsciee.append(wyjscie)
         return np.where(np.array(wyjsciee) > 0.5, 1, 0)
+
+
+
+
+class Linear:
+    def __init__(self, neurony, wejscia):
+        self.m_w = np.random.randn(wejscia, neurony)
+        self.v_b = np.zeros((1, neurony))
+        self.v_X = None
+
+    def forward(self, v_x):
+        self.v_X = v_x
+        #print(self.v_X.shape)
+        return (self.v_X @ self.m_w) + self.v_b
+
+    def backward(self, blad, eta):
+        self.m_w -= eta * (self.v_X.T @ blad)
+        #print(blad,self.v_b,self.v_b - np.sum(blad),np.sum(blad), 'asdasdasdasdasd')
+        self.v_b -= np.sum(eta * blad)#blad
+        return blad @ self.m_w.T
+
+class Activation:
+    def __init__(self):
+        self.s = None
+
+    def forward(self,s):
+        self.s = s
+
+        return logistic(self.s)
+
+    def backward(self, blad,eta):
+        return simoid(logistic(self.s))*blad
 
